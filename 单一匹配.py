65824f657ecb9e3ebee3de342e081b1d84b2d603
Ryan <129248438+dongbenx@users.noreply.github.com>
2024-12-25 15:56:36 +0800

    Update 单一匹配.py

--- 单一匹配.py
+++ 单一匹配.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 # 文件路径
-std_file_path = '/Users/zhuyenan/Downloads/master.xlsx'
-data_file_path = '/Users/zhuyenan/Downloads/上市企业数实融合.xlsx'  # 确保文件扩展名正确
-output_file_path = '/Users/zhuyenan/Downloads/merged_file2.xlsx'
+std_file_path = '/workspaces/stata/数据/panel_data.xlsx'
+data_file_path = '/workspaces/stata/数据/中国宏观经济数据库-年度（分省级） (1).xlsx'  # 确保文件扩展名正确
+output_file_path = '/workspaces/stata/数据/merged_file.xlsx'
 
 # 读取Excel文件
 std_df = pd.read_excel(std_file_path)
@@ -14,3 +14,4 @@ df_main = std_df.merge(data_df, on=['地区', '时间'], how='left')
 
 # 保存合并后的结果
 df_main.to_excel(output_file_path, index=False, engine='openpyxl')
+
